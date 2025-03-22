c4de6b9b4ad165264cab6abd68b08f9c90be5802
Alvin Astradinata <alvnastr@gmail.com>
2025-03-23 00:23:06 +0700

    First Commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Klikmart - Authentication",
+  description: "Klikmart authentication pages",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider>
+        <body className={inter.className}>{children}</body>
+      </ClerkProvider>
+    </html>
+  );
+}
