857d025359570d4b68f8ba38caba60d8d6cdd8aa
Alvin Astradinata <alvnastr@gmail.com>
2025-03-25 22:22:18 +0700

    update layout

---
+++ lib/constants.tsx
@@ -0,0 +1,35 @@
+import {
+    LayoutDashboard,
+    Boxes,
+    ShoppingBag,
+    Tag,
+    UsersRound,
+  } from "lucide-react";
+  
+  export const navLinks = [
+    {
+      url: "/",
+      icon: <LayoutDashboard />,
+      label: "Dashboard",
+    },
+    {
+      url: "/collections",
+      icon: <Boxes />,
+      label: "Collections",
+    },
+    {
+      url: "/products",
+      icon: <Tag />,
+      label: "Products",
+    },
+    {
+      url: "/orders",
+      icon: <ShoppingBag />,
+      label: "Orders",
+    },
+    {
+      url: "/customers",
+      icon: <UsersRound />,
+      label: "Customers",
+    },
+  ];
